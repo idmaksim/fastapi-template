ad579572535ec9b61620b69aab0b0b8db371ef69
idmaksim <dementevmaksim1657@gmail.com>
2025-04-18 00:49:20 +0300

    Remove unused dependency files for category and item services

--- src/dependencies/category_dependency.py
+++
@@ -1,7 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-
-from src.services.category_service import CategoryService, get_category_service
-
-CategoryServiceDep = Annotated[CategoryService, Depends(get_category_service)]
--- src/dependencies/item_dependency.py
+++
@@ -1,7 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-
-from src.services.item_service import ItemService, get_item_service
-
-ItemServiceDep = Annotated[ItemService, Depends(get_item_service)]
