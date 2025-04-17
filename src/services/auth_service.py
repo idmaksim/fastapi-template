7893448e68bc387671a244d32b7f3c2e29f73b57
idmaksim <dementevmaksim1657@gmail.com>
2025-04-18 00:35:38 +0300

    Refactor AuthService to improve token handling and add fastapi-template package to uv.lock

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,11 +1,14 @@
-from datetime import datetime, timedelta
-from src.env import ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET
-from src.dependencies.user_dependency import UserServiceDep
-from src.schemas.auth import LoginRequest, RegisterRequest
-from src.services.user_service import UserService
-from jose import jwt
 import asyncio
+from datetime import datetime, timedelta
 
+from fastapi import HTTPException
+from jose import jwt
+
+from src.dependencies.user_dependency import UserServiceDep
+from src.env import ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET
+from src.schemas.auth import LoginRequest, RegisterRequest
+from src.services.password_service import verify_password
+from src.services.user_service import UserService
 from src.types.jwt_sub import JWTSub
 
 
@@ -13,23 +16,19 @@ class AuthService:
     def __init__(self, user_service: UserService):
         self.user_service = user_service
 
-    async def login(self, body: LoginRequest) -> bool:
+    async def login(self, body: LoginRequest) -> dict[str, str]:
         user = await self.user_service.get_user_by_email(body.email)
-        if not user:
-            return False
-        # if not verify_password(password, user.password):
-        # return False
-        return True
+
+        if not await verify_password(body.password, user.password):
+            raise HTTPException(status_code=401, detail="Invalid credentials")
+
+        return await self.get_all_tokens(JWTSub(id=user.id))
 
     async def register(self, body: RegisterRequest) -> dict[str, str]:
         user = await self.user_service.create_user(body)
         jwt_sub = JWTSub(id=user.id)
 
-        access_token, refresh_token = await asyncio.gather(
-            self.get_access_token(jwt_sub), self.get_refresh_token(jwt_sub)
-        )
-
-        return {"access_token": access_token, "refresh_token": refresh_token}
+        return await self.get_all_tokens(jwt_sub)
 
     async def get_access_token(self, data: JWTSub) -> str:
         return jwt.encode(
@@ -52,6 +51,12 @@ class AuthService:
             key=REFRESH_TOKEN_SECRET,
             algorithm="HS256",
         )
+
+    async def get_all_tokens(self, data: JWTSub) -> dict[str, str]:
+        access_token, refresh_token = await asyncio.gather(
+            self.get_access_token(data), self.get_refresh_token(data)
+        )
+        return {"access_token": access_token, "refresh_token": refresh_token}
 
 
 async def get_auth_service(user_service: UserServiceDep) -> AuthService:
