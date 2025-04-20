799a890f98d5f4c02b692fe5db486fbfc0e0e3d7
idmaksim <dementevmaksim1657@gmail.com>
2025-04-20 22:37:12 +0300

    Add CACHE_TTL environment variable and implement caching for user retrieval endpoint

--- src/env.py
+++ src/env.py
@@ -7,11 +7,13 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 REFRESH_TOKEN_SECRET = os.getenv("REFRESH_TOKEN_SECRET")
+CACHE_TTL = int(os.getenv("CACHE_TTL"))
 
 envs = [
     DATABASE_URL,
     ACCESS_TOKEN_SECRET,
     REFRESH_TOKEN_SECRET,
+    CACHE_TTL,
 ]
 
 
--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 
+from src.env import CACHE_TTL
 from src.dependencies.jwt_dependencies import CurrentUser
 from src.schemas.user import UserResponse
+from fastapi_cache.decorator import cache
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.get("/me", response_model=UserResponse)
+@cache(expire=CACHE_TTL)
 async def get_me(user: CurrentUser):
     return user
